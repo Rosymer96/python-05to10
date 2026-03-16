4016d823c85c0af05653e7e5a6289513d9679518
Rosa Vela <rosavela@macbook-pro.local>
2026-03-16 14:33:31 +0100

    Ejercicios listos para primera presentacion

---
+++ ex6/ft_garden_summary.py
@@ -0,0 +1,4 @@
+def ft_garden_summary():
+    garden_name = input("Enter garden name: ")
+    plants = input("Enter number of plants: ")
+    print(f"Garden: {garden_name}\nPlants: {plants}\nStatus: Growing well!!")
