5f7efaccb5548e2cae51efdf18e21346059df15d
Rosa Vela <rosymer96@gmail.com>
2026-04-15 15:51:04 +0200

    Avance de ejercicio

--- PYTHON-04/ex1/ft_archive_creation.py
+++ PYTHON-04/ex1/ft_archive_creation.py
@@ -2,24 +2,28 @@ import sys
 from typing import IO
 
 
-def ft_read_text(f: IO) -> None:
+def ft_read_text(f: IO) -> str:
     content = f.read()
     print(f"---\n\n{content}\n---")
-
+    return content
 
 def main(args: list) -> None:
     if len(args) == 1:
         print(f"Usage: {args[0]} <file>")
         return
     else:
-        print("=== Cyber Archives Recovery ===")
+        print("=== Cyber Archives Recovery & Preservation ===")
         print(f"Accessing file '{args[1]}'")
         try:
             opened = open(args[1], 'r')
         except (FileNotFoundError, PermissionError, IsADirectoryError) as e:
             print(f"Error opening file '{args[1]}': {e}")
             return
-        ft_read_text(opened)
+        content = ft_read_text(opened)
+        lines = content.split('\n')
+        for line in lines:
+            line = line.append('#\n')
+            print(line)
         opened.close()
         print(f"File '{args[1]}' closed.")
 
